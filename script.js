f63fe53dc0ba6a7d1dc4e28c960736a8332cd269
Peter Murphy <petermurphy@Peters-MacBook-Pro-2.local>
2018-09-06 01:46:20 -0400

    ok JS might be the worse _that_ ever existed

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-var cells;
 var test;
 
 $(function() { //On ready
-    cells = []; //Saves all the cell elements (not just text) in an array
 
-    for(var i = 0; i < 9; i++) {
-      cells[i] = $('#cell-'+ i.toString() );
-    }
-    test = new TicTacToeHashCode("011111111", cells);
-    test.configure();
+    test = new TicTacToeHashCode();
 
-    randomSetup();
 });
 
-//select random values for the starting board
-function randomSetup() {
-  for(var i = 0; i < cells.length; i++) {
-    var ch = getRandomChar();
-    cells[i].text(ch);
-  }
-
-}
-
-//Helper function which generates a random character {X, O}
-function getRandomChar() {
-  var r = Math.floor(Math.random() * 100);
-  console.log(r + " even? " + (r%2 == 0) );
-  if( r % 2 == 0) { return "X"; }
-  else            { return "O"; }
+function randomize() {
+  test.randomize();
 }
