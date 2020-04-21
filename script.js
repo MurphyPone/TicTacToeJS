0a424e1997ceb4d03cd759b20df97c97fde21986
Peter Murphy <murphyP1@bishopireton.org>
2020-04-20 21:05:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var board;
 
 $(function() { //On ready
     board = new TicTacToeHashCode();
-    $('#formula').text('$$\\text{When } t =\\text{the given token\'s individual hash code value [0, 2], and } n = \\text{the token\'s position on the board [0, 8], then the index } i \\text{ \for a unique board configuration is given by the equation: } i = \\sum  3 ^ n t $$');
+    $('#formula').text('$$\\text{When } t =\\text{the given token\'s individual hash code value [0, 2], and } n = \\text{the token\'s position on the board [0, 8], then the index } i \\text{ for a unique board configuration is given by the equation: } i = \\sum  3 ^ n t $$');
 });
 
 function cycle(cell) {
